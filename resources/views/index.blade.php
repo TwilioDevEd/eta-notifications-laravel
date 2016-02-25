2426f666b2f5afa02bd718f19c86f02c49758e1a
Mario Celi <mceli@stackbuilders.com>
2016-02-25 08:54:26 -0500

    add remaining UI changes, remove phone number

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<div class="container-fluid no-paddin">
+<div class="container-fluid">
     <div class="row" id="order-index-header">
         <div class="col-md-1 col-xs-1">
             <span class="order-index-title">ORDERS</span>
@@ -14,22 +14,26 @@
             <img id="order-index-icon" src="/img/small-logo.png"/>
         </div>
     </div>
-    <div class="list-group list-special">
-        @foreach ($orders as $order)
-            <a href="{{ route('order.show', ['id' => $order->id]) }}" class="list-group-item list-special-item">
-                <div class="row">
-                    <div class="col-xs-8 text-uppercase">
-                        {{ $order->customer_name }}
-                    </div>
-                    <div class="col-xs-3 index-status text-uppercase">
-                        {{ $order->status }}
-                    </div>
-                    <div class="col-xs-1">
-                        <i class="fa fa-caret-right"></i>
-                    </div>
-                </div>
-            </a>
-        @endforeach
-    </div>
+    <div class=row>
+        <div class="col-xs-12 no-padding">
+            <div class="list-group list-special">
+                @foreach ($orders as $order)
+                    <a href="{{ route('order.show', ['id' => $order->id]) }}" class="list-group-item list-special-item">
+                        <div class="row">
+                            <div class="col-xs-8 text-uppercase">
+                                {{ $order->customer_name }}
+                            </div>
+                            <div class="col-xs-3 index-status text-uppercase">
+                                {{ $order->status }}
+                            </div>
+                            <div class="col-xs-1">
+                                <i class="fa fa-caret-right"></i>
+                            </div>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    <div>
 </div>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<div class="container-fluid no-padding">
+<div class="container-fluid">
     <div class="row" id="order-index-header">
         <div class="col-md-4 col-xs-4">
             <a href="{{ route('order.index') }}"><i class="fa fa-caret-left order-index-title"></i><span class="order-index-title">BACK</span></a>
@@ -16,21 +16,16 @@
     </div>
     <div class="row">
         <div id="house-image" class="col-md-7 col-xs-7 no-padding">
-            <span>{{ $order->customer_name }}</span>
+            <p id="customer-name-p">{{ $order->customer_name }}</p>
         </div>
         <div id="location-image" class="col-md-5 col-xs-5 no-padding"></div>
     </div>
     <div id="show-info-row" class="row">
-        <div class="col-xs-5">
+        <div class="col-xs-6 no-padding">
             Status: <span class="index-status text-uppercase">{{ $order->status }}</span>
         </div>
-        <div id="show-number" class="col-xs-7">
-            {{ $order->phone_number }}
-        </div>
-    </div>
-    <div id="show-info-row" class="row">
-        <div class="col-xs-12">
-            Notification Status: <span class="index-status text-uppercase">{{ $order->notification_status }}</span>
+        <div class="col-xs-6 no-padding">
+            Notification: <span class="index-status text-uppercase">{{ $order->notification_status }}</span>
         </div>
     </div>
     <div class="row show-button-row">
